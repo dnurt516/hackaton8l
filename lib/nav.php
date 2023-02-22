d759bd50784dede04abbb42cd75552a442c2f4d9
Kirill Lukin <kiryouha.lukin@gmail.com>
2023-02-22 17:33:41 +0600

    Fixing nav

--- lib/nav.php
+++ lib/nav.php
@@ -10,13 +10,15 @@
 		if ($url == 'ajax') {
 			include 'ajax.php';
 		} else {
-			$file = WAY.'classes/'.$html->array_urls[$url];
+			$file = '../classes/'.$html->array_urls[$url];
 
 			if (file_exists($file)) {
 				require_once $file;
 				$class = new $html->array_urls[$url];
 
 				if (method_exists($class, 'Content')) $content .= $class->Content();
+			} else {
+				$content .= 'Error 404';
 			}
 		}
 	} else {
